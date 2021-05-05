4cd25db6b883f7dcfd2c2dbfe74e414f501a4d80
Martin <mbeard@spartaglobal.com>
2021-05-05 14:29:19 +0100

    Visual Studio - empty project

---
+++ TeleportAPITestFramework/TeleportAPITestFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeleportAPITestFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
