a62488a8560c0c42ffd8fbbd64abba25df0d758c
Martin <mbeard@spartaglobal.com>
2021-05-06 10:36:36 +0100

    Two framweworks added: Teleport and Postcodes

---
+++ API_App/API_App/Model.cs
@@ -0,0 +1,198 @@
+﻿
+public class Rootobject
+{
+    public _Embedded _embedded { get; set; }
+    public _Links1 _links { get; set; }
+    public int count { get; set; }
+    public Bounding_Box bounding_box { get; set; }
+    public string continent { get; set; }
+    public string full_name { get; set; }
+    public bool is_government_partner { get; set; }
+    public string mayor { get; set; }
+    public string name { get; set; }
+    public string slug { get; set; }
+    public string teleport_city_url { get; set; }
+    public string ua_id { get; set; }
+}
+
+public class _Embedded
+{
+    public CitySearchResults[] citysearchresults { get; set; }
+}
+
+public class CitySearchResults
+{
+    public _Links _links { get; set; }
+    public object[] matching_alternate_names { get; set; }
+    public string matching_full_name { get; set; }
+
+
+}
+
+public class _Links
+{
+    public CityItem cityitem { get; set; }
+    public Cury[] curies { get; set; }
+    public Self self { get; set; }
+    public UaAdmin1Divisions[] uaadmin1divisions { get; set; }
+    public UaCities uacities { get; set; }
+    public UaContinent uacontinent { get; set; }
+    public UaCountries[] uacountries { get; set; }
+    public UaDetails uadetails { get; set; }
+    public UaIdentifyingCity uaidentifyingcity { get; set; }
+    public UaImages uaimages { get; set; }
+    public UaPrimaryCities[] uaprimarycities { get; set; }
+    public UaSalaries uasalaries { get; set; }
+    public UaScores uascores { get; set; }
+}
+
+public class CityItem
+{
+    public string href { get; set; }
+}
+
+public class _Links1
+{
+    public Cury[] curies { get; set; }
+    public Self self { get; set; }
+}
+
+public class Self
+{
+    public string href { get; set; }
+}
+
+public class Cury
+{
+    public string href { get; set; }
+    public string name { get; set; }
+    public bool templated { get; set; }
+
+}
+
+
+public class UaCities
+{
+    public string href { get; set; }
+}
+
+public class UaContinent
+{
+    public string href { get; set; }
+    public string name { get; set; }
+}
+
+public class UaDetails
+{
+    public string href { get; set; }
+}
+
+public class UaIdentifyingCity
+{
+    public string href { get; set; }
+    public string name { get; set; }
+}
+
+public class UaImages
+{
+    public string href { get; set; }
+}
+
+public class UaSalaries
+{
+    public string href { get; set; }
+}
+
+public class UaScores
+{
+    public string href { get; set; }
+}
+
+
+
+public class UaAdmin1Divisions
+{
+    public string href { get; set; }
+    public string name { get; set; }
+}
+
+public class UaCountries
+{
+    public string href { get; set; }
+    public string name { get; set; }
+}
+
+public class UaPrimaryCities
+{
+    public string href { get; set; }
+    public string name { get; set; }
+}
+
+public class Bounding_Box
+{
+    public Latlon latlon { get; set; }
+}
+
+public class Latlon
+{
+    public float east { get; set; }
+    public float north { get; set; }
+    public float south { get; set; }
+    public float west { get; set; }
+}
+
+
+// Postcodes classes
+
+
+public class Result
+{
+    public string query { get; set; }
+    public Result1 result { get; set; }
+}
+
+public class Result1
+{
+    public string postcode { get; set; }
+    public int quality { get; set; }
+    public int eastings { get; set; }
+    public int northings { get; set; }
+    public string country { get; set; }
+    public string nhs_ha { get; set; }
+    public float longitude { get; set; }
+    public float latitude { get; set; }
+    public string european_electoral_region { get; set; }
+    public string primary_care_trust { get; set; }
+    public string region { get; set; }
+    public string lsoa { get; set; }
+    public string msoa { get; set; }
+    public string incode { get; set; }
+    public string outcode { get; set; }
+    public string parliamentary_constituency { get; set; }
+    public string admin_district { get; set; }
+    public string parish { get; set; }
+    public string admin_county { get; set; }
+    public string admin_ward { get; set; }
+    public string ced { get; set; }
+    public string ccg { get; set; }
+    public string nuts { get; set; }
+    public Codes codes { get; set; }
+}
+
+public class Codes
+{
+    public string admin_district { get; set; }
+    public string admin_county { get; set; }
+    public string admin_ward { get; set; }
+    public string parish { get; set; }
+    public string parliamentary_constituency { get; set; }
+    public string ccg { get; set; }
+    public string ccg_id { get; set; }
+    public string ced { get; set; }
+    public string nuts { get; set; }
+    public string lsoa { get; set; }
+    public string msoa { get; set; }
+    public string lau2 { get; set; }
+}
+
+
---
+++ API_App/API_App/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace API_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var restClient = new RestClient("https://api.teleport.org/");
+            var restRequest = new RestRequest();
+            restRequest.Method = Method.GET;
+            restRequest.AddHeader("Content-Type", "application/json");
+            //Don't timeout
+            restRequest.Timeout = -1;
+
+            var cityId = "geonameid:1796236";
+
+            restRequest.Resource = $"api/cities/{cityId}/";
+
+            var restResponse = restClient.Execute(restRequest);
+
+            //Console.WriteLine("Response content (string):");
+            //Console.WriteLine(restResponse.Content);
+            Console.WriteLine("\n\n\n\n");
+
+            var jsonResponse = JObject.Parse(restResponse.Content);
+            Console.WriteLine("\nRespone content as JObject");
+            Console.WriteLine(jsonResponse);
+
+            var cityName = jsonResponse["name"];
+            var urbanArea = jsonResponse["_links"]["curies"][2]["name"];
+            Console.WriteLine(cityName);
+            Console.WriteLine(urbanArea);
+            var geonameId = jsonResponse["geoname_id"];
+            var lat = jsonResponse["location"]["latlon"]["latitude"];
+            var lon = jsonResponse["location"]["latlon"]["longitude"];
+            Console.WriteLine("\n\n\n\n");
+
+            restClient = new RestClient("https://api.postcodes.io/postcodes");
+
+            restRequest.Method = Method.POST;
+            restRequest.AddHeader("Content-Type", "application/json");
+            //Don't timeout
+            restRequest.Timeout = -1;
+
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Cookie", "__cfduid=d07ff89d6ceb11516cec847fedae7636c1619439989");
+            request.AddParameter("application/json", "{\r\n  \"postcodes\" : [\"PR3 0SG\", \"M45 6GN\", \"EX165BL\"]\r\n}", ParameterType.RequestBody);
+            IRestResponse response = restClient.Execute(request);
+            //Console.WriteLine(response.Content);
+
+
+
+
+
+
+            //Console.WriteLine("Response content (string):");
+            //Console.WriteLine(restResponse.Content);
+            Console.WriteLine("\n\n\n\n");
+            jsonResponse = JObject.Parse(response.Content);
+            Console.WriteLine("\nRespone content as JObject");
+            Console.WriteLine(jsonResponse);
+
+
+            foreach (var i in jsonResponse["result"]) {
+                var adminDistrict = i["result"]["admin_district"];
+                var pPostCode = i["result"]["postcode"];
+                Console.WriteLine(adminDistrict);
+                Console.WriteLine(pPostCode);
+            }
+            //var geonameId = jsonResponse["geoname_id"];
+            //var lat = jsonResponse["location"]["latlon"]["latitude"];
+            //var lon = jsonResponse["location"]["latlon"]["longitude"];
+
+
+
+            // POST example using postcodes
+
+            //var client = new RestClient("https://api.postcodes.io/postcodes");
+            //client.Timeout = -1;
+            //var request = new RestRequest(Method.POST);
+            //request.AddHeader("Content-Type", "application/json");
+            //request.AddHeader("Cookie", "__cfduid=d07ff89d6ceb11516cec847fedae7636c1619439989");
+            //request.AddParameter("application/json", "{\r\n  \"postcodes\" : [\"PR3 0SG\", \"M45 6GN\", \"EX165BL\"]\r\n}", ParameterType.RequestBody);
+            //IRestResponse response = client.Execute(request);
+            //Console.WriteLine(response.Content);
+        }
+    }
+}
